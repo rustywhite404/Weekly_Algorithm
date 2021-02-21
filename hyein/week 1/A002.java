9b11d860fab18b25d4a0b16ef8030290e62f6118
hyein1794 <hyein1794@gmail.com>
2021-02-21 15:42:14 +0900

    두번째 알고리즘 문제

---
+++ hyein/week 1/A002.java
@@ -0,0 +1,19 @@
+package hyein;
+
+public class A002 {
+
+	public static void main(String[] args) {
+		// 숫자 1, 24, 23, 6, 15 중에서 짝수만 출력하기 
+		
+		int[] num = {1, 24, 23, 6, 15};
+		
+		for (int i = 0; i < num.length; i++) {
+			if (num[i]%2==0) {
+				System.out.println(num[i]);
+			};
+		}
+		
+		
+	}
+
+}
