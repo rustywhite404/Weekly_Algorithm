4b21977753617e05518a4a8bb7026a210ae7e01d
soyoeee <cfpe18@gmail.com>
2021-10-28 14:21:14 +0900

    소영 폴더 추가

--- soyeong/week1/test.java
+++ soyeong/week1/test.java
@@ -3,8 +3,5 @@ package week1;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
 	}
 }
